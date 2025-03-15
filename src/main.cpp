76857b314747e80d94252e4ccfbc94a7d4f61eff
Zero-zany <ZZY990613@outlook.com>
2025-03-15 16:03:49 +0800

    修改了主函数的头文件链接问题

--- src/main.cpp
+++ src/main.cpp
@@ -1,2 +1,26 @@
+#include <iostream>
 
-int main(int argc, char const *argv[]) { return 0; }
+extern "C" {
+#include <libavutil/log.h>
+#include <libavutil/mem.h>
+}
+
+int main(int argc, char const* argv[])
+{
+    av_log_set_level(AV_LOG_DEBUG);
+    int* ptr = static_cast<int*>(av_malloc(sizeof(int)));
+    *ptr     = 101;
+    std::cout << *ptr << '\n';
+    av_free(static_cast<void*>(ptr));
+    std::cout << *ptr << '\n';
+    ptr = nullptr;
+    if (!ptr)
+    {
+        std::cout << "free" << '\n';
+    }
+    else
+    {
+        std::cout << "no free" << '\n';
+    }
+    return 0;
+}
